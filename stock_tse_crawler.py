4278ebc030a230b2a3d29c4f219cdcf52f0ae85b
economic <economic@thu.edu.tw>
2022-08-18 16:10:14 +0800

    adjust format & db test

--- stock_tse_crawler.py
+++ stock_tse_crawler.py
@@ -50,7 +50,7 @@ class StockCrawling:
             host="127.0.0.1",  # 主機名
             user="thu",  # 用戶名
             passwd="Thu@201905",  # 密碼
-            db="mydb",  # 數據庫名稱
+            db="stocktescrawler",  # 數據庫名稱
             charset="utf8")
 
     def close_db(self):
@@ -63,35 +63,71 @@ class StockCrawling:
             return
 
         cursor = self.db.cursor()
-        database = "mydb"
-        # d: 日期, h: 最高, l:最低, c:代號, n: 名稱, t:時間, o: 開盤, v: 交易量, z: 成交價
+        database = "stocktescrawler"
+
         ''' ### SQL Table tse_stock
-        `tse_stock`.`sdatetime`,
-        `tse_stock`.`sid`,
-        `tse_stock`.`sname`,
-        `tse_stock`.`price`,
-        `tse_stock`.`high`,
-        `tse_stock`.`low`,
-        `tse_stock`.`open`,
-        `tse_stock`.`amount`
+        `tse_stock`.`sdate`,            # 日期
+        `tse_stock`.`stime`,            # 時間
+        `tse_stock`.`sid`,              # 股票代碼
+        `tse_stock`.`sname`,            # 股票簡稱
+        `tse_stock`.`price`,            # 當盤成交價
+        `tse_stock`.`thisamount`        # 當盤成交量
+        `tse_stock`.`high`,             # 最高價
+        `tse_stock`.`low`,              # 最低價
+        `tse_stock`.`yesterday`,        # 昨收
+        `tse_stock`.`open`,             # 開盤價
+        `tse_stock`.`amount`            # 累積成交量
         '''
 
         if stock_dict is None:
             return None
 
-        sql = "INSERT IGNORE INTO {}.tse_stock(sdatetime, sid, sname, price, high, low, open, amount) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)".format(
-            database)
+        sql = "INSERT IGNORE INTO {}.tse_stock(sdate, stime, sid, sname, price, thisamount, high, low, yesterday, open, amount) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)".format(database)
         records = []
         # print(stock_dict)
         for s_dict in stock_dict:
             # print(s_dict)
             try:
-                _t = dt.datetime.strptime(s_dict['t'], '%H:%M:%S').time()
-                _dt = dt.datetime.combine(dt.datetime.strptime(s_dict['d'], "%Y%m%d").date(), _t).strftime(
-                    '%Y-%m-%d %H:%M:%S')
-                records.append([_dt, s_dict['c'], s_dict['n'],
+                #_dt: 日期；_t: 時間；c: 股票代碼；n: 股票簡稱；z: 當盤成交價；tv: 當盤成交量；
+                # h: 最高價；l: 最低價；y: 昨收；o: 開盤價；v: 累積成交量
+
+                _dt = dt.datetime.strptime(s_dict['d'], "%Y%m%d").date().strftime('%Y-%m-%d')
+                _t = dt.datetime.strptime(s_dict['t'], '%H:%M:%S').time().strftime('%H:%M:%S')
+                #_dt = dt.datetime.combine(dt.datetime.strptime(s_dict['d'], "%Y%m%d").date(), _t).strftime('%Y-%m-%d %H:%M:%S')
+                '''
+                records.append([_dt, _t, s_dict['c'], s_dict['n'],
                                 float(s_dict['z']), float(s_dict['h']), float(s_dict['l']), float(s_dict['o']),
                                 int(s_dict['v'])])
+                '''
+                if(s_dict['z']!='-'):
+                    s_dict['z'] = float(s_dict['z'])
+                else:
+                    s_dict['z'] = float('NaN')
+                if(s_dict['tv']!='-'):
+                    s_dict['tv'] = float(s_dict['tv'])
+                else:
+                    s_dict['tv'] = float('NaN')
+                if(s_dict['h']!='-'):
+                    s_dict['h'] = float(s_dict['h'])
+                else:
+                    s_dict['h'] = float('NaN')
+                if(s_dict['l']!='-'):
+                    s_dict['l'] = float(s_dict['l'])
+                else:
+                    s_dict['l'] = float('NaN')
+                if(s_dict['o']!='-'):
+                    s_dict['o'] = float(s_dict['o'])
+                else:
+                    s_dict['o'] = float('NaN')
+                if(s_dict['v']!='-'):
+                    s_dict['v'] = float(s_dict['v'])
+                else:
+                    s_dict['v'] = float('NaN')
+
+                records.append([_dt, _t, s_dict['c'], s_dict['n'], s_dict['z'], s_dict['tv'],
+                s_dict['h'], s_dict['l'], float(s_dict['y']), float(s_dict['o']), int(s_dict['v'])])
+
+                
             except ValueError as _e:
                 print("ValueErr : " + str(_e))
 
@@ -108,14 +144,15 @@ class StockCrawling:
 if __name__ == '__main__':
     # To ill stock IDs that you want.
     stock_ids = ['2388', '2380']
-    STOP_TIME_HOUR = 13
-    STOP_TIME_MINUTE = 40
-    STOP_TIME = dt.datetime.now(hour=STOP_TIME_HOUR, minute=STOP_TIME_MINUTE)
+    #STOP_TIME_HOUR = 13
+    #STOP_TIME_MINUTE = 40
+    #STOP_TIME = dt.datetime.now(hour=STOP_TIME_HOUR, minute=STOP_TIME_MINUTE)
+    STOP_TIME = 134000
     crawling = StockCrawling()
     try:
         while True:
-            now = dt.datetime.now()
-            if now < STOP_TIME:
+            now = dt.datetime.now().strftime("%H%M%S")
+            if int(now) < STOP_TIME:
                 data = crawling.show_realtime(*stock_ids)
                 crawling.insert_sql(data)
                 _sleep_time = random.randint(2, 4)
